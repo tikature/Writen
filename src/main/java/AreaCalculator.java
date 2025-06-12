ce79e7610f3b716c5b0aa4c1deb9c08db5ca9832
hyeranissa <hyeranissa@gmail.com>
2025-06-13 05:44:38 +0700

    Update AreaCalculator.java

--- src/main/java/AreaCalculator.java
+++ src/main/java/AreaCalculator.java
@@ -14,7 +14,7 @@ public class AreaCalculator {
 					throw new IllegalArgumentException("This SQUARE needs 1 dimension (side)");
 				}
 				return dimensions[0] * dimensions[0];
-			case RECTANGLE:
+			case RECTANGLEz:
 				if (dimensions.length != 2) {
 					throw new IllegalArgumentException("This RECTANGLE needs 2 dimensions (length and width)");
 				}
@@ -25,8 +25,8 @@ public class AreaCalculator {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Luas Lingkaran (r=3): " + calculateArea(ShapeType.CIRCLE, 3));
-		System.out.println("Luas Persegi (s=4): " + calculateArea(ShapeType.SQUARE, 4));
+		System.out.println("Luas Lingkaran (r=3): " + calculateArea(ShapeType.CIRCLESS, 3));
+		System.out.println("Luas Persegi (s=4): " + calculateArea(ShapeType.SQUARES, 4));
 		System.out.println("Luas Persegi Panjang (l=5, w=2): " + calculateArea(ShapeType.RECTANGLE, 5, 2));
 	}
 }
