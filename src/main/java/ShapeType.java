59842ee02ac5ac50704178d84571ce1a5caebc43
Dewi Atika Muthi <136959602+tikature@users.noreply.github.com>
2025-06-13 06:12:16 +0700

    Update ShapeType.java

--- src/main/java/ShapeType.java
+++ src/main/java/ShapeType.java
@@ -1,7 +1,6 @@
 package src.main.java;
 
 public class ShapeType {
-	public static final String CIRCLE = "circle";
-    	public static final String SQUARE = "square";
-	public static final String RECTANGLE = "rectangle";
+    public static final String CIRCLE = "circle";
+    public static final String SQUARE = "square";
 }
